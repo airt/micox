99a41a153323785a8ee23ff5ab83f9b8d05eca9a
airt <airtial@gmail.com>
2019-05-22 15:39:35 +0800

    feat: freestanding binary

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+#![no_std]
+#![no_main]
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+  loop {}
+}
+
+#[panic_handler]
+fn panic(_: &::core::panic::PanicInfo) -> ! {
+  loop {}
+}

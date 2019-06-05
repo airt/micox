0a92aa79e2487924a119b12d7118f949383c83d2
airt <airtial@gmail.com>
2019-06-05 12:02:02 +0800

    feat: allocator designs

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 #![cfg_attr(test, no_main)]
 #![feature(abi_x86_interrupt)]
 #![feature(alloc_error_handler)]
+#![feature(const_fn)]
+#![feature(const_in_array_repeat_expressions)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
